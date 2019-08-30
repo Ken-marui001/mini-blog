60b86c38090b944186417d45ceb7b481cb63db3f
ken-marui001 <noobgladiator001@gmail.com>
2019-08-30 14:51:35 +0900

    define articles#new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,20 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @articles = [*1..20]
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(created_params)
+  end
+
+  private
+  def created_params
+    return params.require(:article).permit(:title, :content, :image).merge(user_id: current_user.id)
   end
 end
